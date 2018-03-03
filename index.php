607e88855f7c606445d4a1c54bac1cd5f5a03a83
TornOne <ott.adermann@gmail.com>
2018-03-04 00:19:58 +0200

    Prototype vol 1

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+include 'pieces/header.php';
+?>
+
+<div class="main">
+	<div class="ranking">
+		<!--TODO:
+		Make elements scale down as browser gets narrower to always fit them.
+		Name and tags should be properly clipped before the other columns start to be affected
+		Maybe? add a color to the weighted review%
+		-->
+		<?php
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		include 'pieces/gameListing.php';
+		?>
+	</div>
+</div>
+
+<?php
+include 'pieces/footer.php';
+?>
