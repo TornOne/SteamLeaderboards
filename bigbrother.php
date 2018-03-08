cde7b06b51c90447de8ed1202b46bb8af5974a65
Oskar Lahesoo <oskar@kodukas.eu>
2018-03-08 15:46:53 +0200

    Still debugging bigbrother.php

--- bigbrother.php
+++ bigbrother.php
@@ -25,11 +25,27 @@ pg_execute($conn, "insert_bb", array($browser, $requestTime, $platform));
 pg_prepare($conn, "get_browsers", 'SELECT * FROM bb_browsers;');
 $result_browsers = pg_execute($conn, "get_browsers", array());
 
-var_dump(pg_fetch_all($result_browsers));
-echo nl2br("\n\n");
-print_r(pg_fetch_all($result_browsers));
+pg_prepare($conn, "get_platforms", 'SELECT * FROM bb_platforms');
+$result_platforms = pg_execute($conn, "get_platforms", array());
+
+pg_prepare($conn, "get_times", 'SELECT * FROM bb_times');
+$result_times = pg_execute($conn, "get_times", array());
+
+while ($row = pg_fetch_row($result_browsers)) {
+    echo nl2br("$row[0]:  $row[1]\n");
+}
+nl2br("\n");
+while ($row = pg_fetch_row($result_platforms)) {
+    echo nl2br("$row[0]:  $row[1]\n");
+}
+nl2br("\n");
+while ($row = pg_fetch_row($result_times)) {
+    echo nl2br("$row[0]:  $row[1]");
+}
 
 pg_free_result($result_browsers);
+pg_free_result($result_platforms);
+pg_free_result($result_times);
 pg_close($conn);
 
 ?>
