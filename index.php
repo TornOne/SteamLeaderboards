62aa0a2796802ef852ebcc96844b1852dc87f82c
Torn <ott.adermann@gmail.com>
2018-04-04 00:40:32 +0300

    Can't fix stupid

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $conn = pg_connect(getenv("DATABASE_URL"));
 $last_refresh = strtotime(pg_fetch_result(pg_query($conn, 'SELECT * FROM last_refresh;'), 0, 0));
 if (time() - $last_refresh > 86400) { //If the last refresh was more than a day ago
 	//pg_query($conn, 'SELECT reset_refresh_time();');
-	exec("python /app/scraper/Test.py" . " 2>&1");//" > dev/null &");
+	echo exec("python scraper/Test.py" . " 2>&1");//" > dev/null &");
 }
 ?>
 
