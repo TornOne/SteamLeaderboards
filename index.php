86b7127fe2255e15160ea34b2d7f247e18cade98
TornOne <ott.adermann@gmail.com>
2018-04-16 21:13:24 +0300

    smolboye Scraper Starter

--- index.php
+++ index.php
@@ -2,27 +2,13 @@
 $conn = pg_connect(getenv("DATABASE_URL"));
 $next_refresh = strtotime(pg_fetch_result(pg_query($conn, "SELECT value FROM config_dates WHERE key='attempted_refresh_time';"), 0, 0)) + 86400 - time(); //Add a day to the last refresh
 if ($next_refresh < 0) {
-    $timeout = 7500; // Dyno timeout in seconds
-    $url = "https://api.heroku.com/apps/steamleaderboards/dynos";
-    $data = array("command" => "scraper", "size" => "free", "type" => "run", "time_to_live" => $timeout);
-    $options = array(
-        "http" => array(
-            "header" => array(
-                "Content-Type" => "application/x-www-form-urlencoded",
-                "Authorization" => "Bearer $API_TOKEN",
-                "Accept" => "application/vnd.heroku+json; version=3"
-            ),
-            "method" => "POST",
-            "content" => http_build_query($data)
-        )
-    );
-    $context = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) {
-        pg_query($conn, 'SELECT update_failed_refresh_time();');
-    } else {
-        pg_query($conn, 'SELECT update_attempted_refresh_time();');
-    }
+	$return_code = exec("python scraper/Start.py");
+	echo $return_code;
+	if ($return_code == "201 Created") {
+		pg_query($conn, 'SELECT update_attempted_refresh_time();');
+	} else {
+		pg_query($conn, 'SELECT update_failed_refresh_time();');
+	}
 }
 ?>
 <?php include 'pieces/head.php'; ?>
