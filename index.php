4bd051b3e2b1a5bbed148771eda118178f31a34f
Oskar Lahesoo <oskar@kodukas.eu>
2018-04-16 18:16:09 +0300

    Fixing scraper stuff

--- index.php
+++ index.php
@@ -3,25 +3,28 @@ $conn = pg_connect(getenv("DATABASE_URL"));
 $next_refresh = strtotime(pg_fetch_result(pg_query($conn, "SELECT value FROM config_dates WHERE key='attempted_refresh_time';"), 0, 0)) + 86400 - time(); //Add a day to the last refresh
 if ($next_refresh < 0) {
     $timeout = 7500; // Dyno timeout in seconds
-    $r = new HttpRequest("https://api.heroku.com/apps/steamleaderboards/dynos", HttpRequest::METH_POST);
-    $r->addPostFields(array("command" => "scraper", "size" => "free", "type" => "run", "timeout" => $timeout));
-    try {
-        $r->send();
-        if ($r->getResponseCode() == 200) {
-            pg_query($conn, 'SELECT update_attempted_refresh_time();');
-        } else {
-            pg_query($conn, 'SELECT update_failed_refresh_time();');
-        }
-    } catch (HttpException $ex) {
+    $url = "https://api.heroku.com/apps/steamleaderboards/dynos";
+    $data = array("command" => "scraper", "size" => "free", "type" => "run", "timeout" => $timeout);
+    $options = array(
+        "http" => array(
+            "method" => "POST",
+            "content" => http_build_query($data)
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result === FALSE) {
         pg_query($conn, 'SELECT update_failed_refresh_time();');
+    } else {
+        pg_query($conn, 'SELECT update_attempted_refresh_time();');
     }
 }
 ?>
-<?php include 'pieces/head.php';?>
+<?php include 'pieces/head.php'; ?>
 <script src="/scripts/index.js"></script>
 <link href="index.css" rel="stylesheet" type="text/css"/>
 </head>
-<?php include 'pieces/header.php';?>
+<?php include 'pieces/header.php'; ?>
 
 <div class="main">
     <!--TODO:
@@ -31,7 +34,7 @@ if ($next_refresh < 0) {
 	Add tags back
 	Add a price to the right edge of the name? (being the first to be hidden)
 	-->
-	<?php include 'pieces/ranking.php';?>
+    <?php include 'pieces/ranking.php'; ?>
 </div>
 
-<?php include 'pieces/footer.php';?>
+<?php include 'pieces/footer.php'; ?>
