f8765643030a57f0ecb31bbe8160235ab1757e18
Oskar Lahesoo <oskar@kodukas.eu>
2018-04-16 17:28:41 +0300

    Fixing scraper stuff

--- index.php
+++ index.php
@@ -2,14 +2,19 @@
 $conn = pg_connect(getenv("DATABASE_URL"));
 $next_refresh = strtotime(pg_fetch_result(pg_query($conn, "SELECT value FROM config_dates WHERE key='attempted_refresh_time';"), 0, 0)) + 86400 - time(); //Add a day to the last refresh
 if ($next_refresh < 0) {
-	$output = array(); //Throwaway
-	$exit_code = 1;
-	exec("python scraper/Scraper.py" . " > /dev/null &",$output,$exit_code);
-	if ($exit_code == 0) {
-		pg_query($conn, 'SELECT update_attempted_refresh_time();');
-	} else {
-		pg_query($conn, 'SELECT update_failed_refresh_time();');
-	}
+    $timeout = 7500; // Dyno timeout in seconds
+    $r = new HttpRequest("https://api.heroku.com/apps/steamleaderboards/dynos", HttpRequest::METH_POST);
+    $r->addPostFields(array("command" => "scraper", "size" => "free", "type" => "run", "timeout" => $timeout));
+    try {
+        $r->send();
+        if ($r->getResponseCode() == 200) {
+            pg_query($conn, 'SELECT update_attempted_refresh_time();');
+        } else {
+            pg_query($conn, 'SELECT update_failed_refresh_time();');
+        }
+    } catch (HttpException $ex) {
+        pg_query($conn, 'SELECT update_failed_refresh_time();');
+    }
 }
 ?>
 <?php include 'pieces/head.php';?>
