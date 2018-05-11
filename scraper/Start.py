3c29fca36d11db8fd4adb909fdda75dd3a7d5114
TornOne <ott.adermann@gmail.com>
2018-05-11 17:53:11 +0300

    Lowered the TTL of the scraper dyno to 30 minutes

--- scraper/Start.py
+++ scraper/Start.py
@@ -1,5 +1,5 @@
 import httplib, os
 conn = httplib.HTTPSConnection("api.heroku.com")
-conn.request("POST", "/apps/steamleaderboards/dynos", '{"type": "run", "time_to_live": 7500, "command": "scraper", "size": "free"}', {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + os.environ['API_TOKEN'], 'Accept': 'application/vnd.heroku+json; version=3'})
+conn.request("POST", "/apps/steamleaderboards/dynos", '{"type": "run", "time_to_live": 1800, "command": "scraper", "size": "free"}', {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + os.environ['API_TOKEN'], 'Accept': 'application/vnd.heroku+json; version=3'})
 response = conn.getresponse()
 print response.status, response.reason
