9aa709a3ff1c8f1a6bf8eb1f986b76ed7eff150c
TornOne <ott.adermann@gmail.com>
2018-05-17 10:31:03 +0300

    Server side search implemented

--- pieces/ranking.php
+++ pieces/ranking.php
@@ -3,11 +3,87 @@
 	if (!isset($conn)) {
 		$conn = pg_connect(getenv("DATABASE_URL"));
 	}
+	$where_fields = array();
+	$where_params = array();
 
-	$page_count = ceil(pg_fetch_result(pg_query($conn, "SELECT COUNT(*) FROM games;"), 0, 0) / 25);
+	//Tags
+	if (isset($_GET["tags"])) {
+		$where_fields[] = "tags @>";
+		$where_params[] = "{" . $_GET["tags"] . "}";
+	}
+	//Price
+	if (isset($_GET["price"])) {
+		$where_fields[] = "price <=";
+		$where_params[] = $_GET["price"];
+	}
+	//Date
+	if (isset($_GET["date"])) {
+		$date_preset = $_GET["date"];
+		if ($date_preset === "week") {
+			$_GET["from"] = date("Y-m-d", time() - 604800);
+			$_GET["to"] = date("Y-m-d");
+		} else if ($date_preset === "week2") {
+			$_GET["from"] = date("Y-m-d", time() - 1209600);
+			$_GET["to"] = date("Y-m-d", time() - 604800);
+		} else if ($date_preset === "month") {
+			$_GET["from"] = date("Y-m-d", time() - 2592000);
+			$_GET["to"] = date("Y-m-d");
+		} else if ($date_preset === "season") {
+			$_GET["from"] = date("Y-m-d", time() - 7862400);
+			$_GET["to"] = date("Y-m-d");
+		} else if ($date_preset === "year") {
+			$_GET["from"] = date("Y-m-d", time() - 31536000);
+			$_GET["to"] = date("Y-m-d");
+		}
+	}
+	if (isset($_GET["from"])) {
+		$where_fields[] = "release >=";
+		$where_params[] = $_GET["from"];
+	}
+	if (isset($_GET["to"])) {
+		$where_fields[] = "release <=";
+		$where_params[] = $_GET["to"];
+	}
+	//Platforms
+	if (isset($_GET["vr"])) {
+		$where_fields[] = "vr =";
+		$where_params[] = "t";
+	}
+	if (isset($_GET["win"])) {
+		$where_fields[] = "windows =";
+		$where_params[] = "t";
+	}
+	if (isset($_GET["mac"])) {
+		$where_fields[] = "mac =";
+		$where_params[] = "t";
+	}
+	if (isset($_GET["linux"])) {
+		$where_fields[] = "linux =";
+		$where_params[] = "t";
+	}
+
+	$where_query = "";
+	for ($i = 0, $c = count($where_fields); $i < $c; $i++) {
+		if ($i == 0) {
+			$where_query .= " WHERE ";
+		} else {
+			$where_query .= " AND ";
+		}
+		$where_query .= $where_fields[$i] . " $" . ($i + 1);
+	}
+
+	$page_count = ceil(pg_fetch_result(pg_query_params($conn, "SELECT COUNT(*) FROM games" . $where_query . ";", $where_params), 0, 0) / 25);
 	if (isset($_GET["page"]) && $_GET["page"] > 0) {
 		$pagenr = $_GET["page"];
 		if ($pagenr > $page_count) {
+			if ($page_count == 0) {
+				?>
+				<div class="game_listing">
+					<div class="game_title_tags">No games found</div>
+				</div>
+				<?php
+				$page_count = 1;
+			}
 			$pagenr = $page_count;
 		}
 	} else {
@@ -15,7 +91,9 @@
 	}
 
 	$offset = ($pagenr - 1) * 25;
-	$games = pg_query_params($conn, 'SELECT * FROM top_games LIMIT 25 OFFSET $1;', array($offset));
+	$where_params[] = $offset;
+	$games = pg_query_params($conn, "SELECT * FROM top_games" . $where_query . " LIMIT 25 OFFSET $" . count($where_params) . ";", $where_params);
+
 	while ($row = pg_fetch_row($games)) {
 		$offset++;
 		//appid, name, rating, votes, score, windows, mac, linux, vr, release, price, tags
@@ -83,7 +161,7 @@
 
 	<div class="page_selector">
 		<?php if ($pagenr != 1) { ?>
-			<a class="page_button" href="/index.php?page=<?=$pagenr - 1?>" onclick="parseLoadPage(this); return false;"><</a><a href="/index.php?page=1" onclick="parseLoadPage(this); return false;">1</a>
+			<a class="page_button" href="/?page=<?=$pagenr - 1?>" onclick="parseLoadPage(this); return false;"><</a><a href="/?page=1" onclick="parseLoadPage(this); return false;">1</a>
 		<?php }
 
 		if ($pagenr > 4) {
@@ -91,11 +169,11 @@
 		}
 
 		for ($i = max(2, $pagenr - 2); $i < $pagenr; $i++) { ?>
-			<a href="/index.php?page=<?=$i?>" onclick="parseLoadPage(this); return false;"><?=$i?></a>
+			<a href="/?page=<?=$i?>" onclick="parseLoadPage(this); return false;"><?=$i?></a>
 		<?php }
 		echo "<span>$pagenr</span>";
 		for ($i = $pagenr + 1; $i < min($pagenr + 3, $page_count); $i++) { ?>
-			<a href="/index.php?page=<?=$i?>" onclick="parseLoadPage(this); return false;"><?=$i?></a>
+			<a href="/?page=<?=$i?>" onclick="parseLoadPage(this); return false;"><?=$i?></a>
 		<?php }
 
 		if ($pagenr < $page_count - 3) {
@@ -103,7 +181,7 @@
 		}
 
 		if ($pagenr != $page_count) { ?>
-			<a href="/index.php?page=<?=$page_count?>" onclick="parseLoadPage(this); return false;"><?=$page_count?></a><a class="page_button" href="/index.php?page=<?=$pagenr + 1?>" onclick="parseLoadPage(this); return false;">></a>
+			<a href="/?page=<?=$page_count?>" onclick="parseLoadPage(this); return false;"><?=$page_count?></a><a class="page_button" href="/?page=<?=$pagenr + 1?>" onclick="parseLoadPage(this); return false;">></a>
 		<?php } ?>
 	</div>
 </div>
