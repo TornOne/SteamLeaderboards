c35e9966c37c457807e57dc729e5d8f7cf27d00f
TornOne <ott.adermann@gmail.com>
2019-06-27 09:41:09 +0300

    Better debugging info

--- scraper/Scraper.py
+++ scraper/Scraper.py
@@ -128,9 +128,11 @@ def firstPass():
 def getPreciseScore(game):
     if game[3] < 100: #Our initial guess is already exact
         return (game[2], game[3], game[4])
-    print(game[0])
     page = urllib.urlopen("http://store.steampowered.com/appreviews/" + game[0] + "?json=1&filter=all&language=all&review_type=all&purchase_type=" + ("all" if game[10] <= 0 else "steam"))
-    contents = json.loads(page.read())
+    try:
+        contents = json.loads(page.read())
+    except Exception e:
+        print(game[0], e)
     game[3] = contents["query_summary"]["total_reviews"]
     if game[3] != 0:
         game[2] = float(contents["query_summary"]["total_positive"]) / game[3]
