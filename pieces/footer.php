c697c3d3392f4e6880b8e33b5ce9f17619a5b88b
TornOne <ott.adermann@gmail.com>
2019-07-07 09:26:43 +0300

    Footer update apology text

--- pieces/footer.php
+++ pieces/footer.php
@@ -14,6 +14,9 @@
 	?>
 	Last update completed <?=secondsToHuman($last_refresh)?> ago
 	<br/>
+	<i style="font-size: 0.9em">I am aware of the consistently failing updates and have looked into the issue. Sadly, this is not a fix I can make in just a few hours.<br/>
+	I am working on a full rewrite for the site along with a migration to a paid server, hopefully in 2019. This site has not been forgotten.</i>
+	<br/>
 	Next update in <?=secondsToHuman($next_refresh)?>
 	<br/>
 	Page generated in <?=number_format(microtime(true) - $load_start_time, 3)?>s
